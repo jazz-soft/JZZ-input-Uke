645f7e2b14b0ec306cce7782c2bdb51972459d8f
Sema <jazz-dev@jazz-soft.net>
2022-03-18 23:30:52 -0400

    New release

--- javascript/JZZ.input.Uke.js
+++ javascript/JZZ.input.Uke.js
@@ -13,7 +13,7 @@
   if (!JZZ) return;
   if (!JZZ.input) JZZ.input = {};
 
-  var _version = '0.0.9';
+  var _version = '0.1.0';
   function _name(name, deflt) { return name ? name : deflt; }
 
   var i;
