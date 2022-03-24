98834791e6b990e0f384bc49b780d2320edc65a0
Sema <jazz-dev@jazz-soft.net>
2022-03-24 00:09:28 -0400

    Simulate mouse events

--- test/fake-svg.js
+++ test/fake-svg.js
@@ -5,10 +5,11 @@ if (!svg || !svg.createSVGPoint) {
   document.__createElementNS = document.createElementNS;
   document.createElementNS = function(ns, name) {
     node = document.__createElementNS(ns, name);
-    if (ns == _svgns && name == 'svg') {
+    if (ns == _svgns) {
       node.createSVGPoint = function() { return document.createElementNS(_svgns, 'point'); };
+      node.getScreenCTM = function() { return { inverse: function() {} }; };
+      node.matrixTransform = function() { return { x: this.x / 100, y: this.y / 100 }; };
     }
     return node;
   };
 }
-
